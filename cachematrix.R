81a13a849e597a5c422343ed58d91aed8bd02444
tlawotkd <tlawotkd@hanmail.net>
2015-09-28 06:23:36 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -9,19 +9,26 @@
 ## using the target matrix as input argument.
 
 makeCacheMatrix <- function(x = matrix()) {
+	## initialize	
 	result <- NULL 
-		
-      set <- function(y) {
+	
+	## setting the un-inversed matrix	
+      	set <- function(y) {
 		x <<- y
             result <<- NULL
 		## <<- stands for "SUPERassignment", i.e. assign to global var.
 	}
+	
+	## return the un-inversed matrix
 	get <- function() x
 
-	## store the inversed matrix to the result var.
+	## store the inverse var. (supposedly the inversed matrix) to the result var.
 	setInv <- function(inverse) result <<- inverse
+	
 	## return result
 	getInv <- function() result
+	
+	## Now set up the list for the function to work
 	list(set = set, get = get,
 		setInv = setInv,
 		getInv = getInv)
@@ -41,7 +48,7 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
 	
 	## First try to call the above fuction's list to call its 'chached matrix' ,
-	## if it is same as x, just return the cashed one
+	## if it is same as x, just return the cached one
 	m <- x$getInv()
 	if(!is.null(m)) {
 		message("getting cached data")
@@ -52,7 +59,11 @@ cacheSolve <- function(x, ...) {
 	message("Could not get cached data, calculating inverse now")
 	data <- x$get()
 	m <- solve(data, ...)
+	
+	## and cache that solved inverse matrix (i.e. m)
 	x$setInv(m)
+	
+	## and also, return the result as well
 	m
 }
 
